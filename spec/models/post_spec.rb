e2275e6f111136f9a61be60165395ebccb1ab7bf
Andrea Dal Ponte <info@andreadalponte.com>
2015-08-19 14:46:51 +0200

    Post validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    let(:post) { Post.new }
+
+    it 'should not valid after initialization' do
+      expect(post.valid?).to be_falsey
+    end
+
+    it 'should valid with only title and description' do
+      post.title = 'sample title'
+      post.description = 'sample description'
+
+      expect(post.valid?).to be_truthy
+    end
+  end
 end
